c1af30fd665c9e180dcdf554e1b6164289642f3e
trakons <isztl.david@gmail.com>
2015-04-13 14:58:33 +0200

    Create scanner.py

---
+++ scanner.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from socket import * 
+
+if __name__ == '__main__':
+	target = raw_input('Enter host to scan: ')
+    	targetIP = gethostbyname(target)
+    	print 'Starting scan on host ', targetIP
+    	array = ['21','25','80','139','3389','1723','1521','23','110','445','8080','5900','1433','3306']
+    	arraynomi = ['FTP','SMTP','HTTP','NETBIOS','RDP','VPN','Oracle DB','TELNET','POP3','Windows file Sharing','firewall','VNC','MSQL','Mysql']
+    	#scan reserved ports
+    	for i in range(0,len(array)):
+        	s = socket(AF_INET, SOCK_STREAM)
+		s.settimeout(1)
+        	result = s.connect_ex((targetIP, int(array[i])))
+
+        	if(result == 0) :
+            		print "Port", array[i], arraynomi[i], "OPEN"
+		else:
+			print "Port", array[i], arraynomi[i], "CLOSE"
+        	s.close()
